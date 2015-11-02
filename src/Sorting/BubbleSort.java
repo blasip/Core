47fc31c362b29683aa2d6e4b275a1688fc0720d9
blasip <pauldblasi@yahoo.com>
2015-11-02 13:50:09 -0700

    For debugging

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,5 +1,54 @@
 package Sorting;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BubbleSort {
+
+	public void bubbleSort(List<Integer> numbers) {
+
+		int size = numbers.size();
+		for (int i = 0; i < size; i++) {
+			if (i == numbers.size() - 1) {
+				size = size - 1;
+				i = 0;
+				if (size == 0) {
+					break;
+				}
+			}
+			if (numbers.get(i) > numbers.get(i + 1)) {
+				int swapSpace = numbers.get(i);
+				numbers.set(i, numbers.get(i + 1));
+				numbers.set(i + 1, swapSpace);
+
+			}
+			System.out.println(numbers);
+		}
+	}
+
+	@Test
+	public void testBubbleSort() {
+		final List<Integer> numbers = Arrays.asList(4, 2, 1, 6, 3, 5, 4);
+		final List<Integer> expected = Arrays.asList(1, 2, 3, 4, 4, 5, 6);
+
+		bubbleSort(numbers);
+
+		assertEquals(expected, numbers);
+
+	}
+
+	// @Test
+	// public void testInsertSort(){
+	// final List<Integer> numbers = Arrays.asList(4,2,1,6,3,5,4);
+	// final List<Integer> expected = Arrays.asList(1,2,3,4,4,5,6);
+	//
+	// ArrayList<Integer> newArrayList = insertSort(numbers);
+	//
+	// assertEquals(expected,newArrayList);
+	// }
 
 }
