30e9ad2976f0b63e703ca9e0e973ae414ce8864c
CarlaLThomspon <CarlaLThompson@aol.com>
2015-11-05 13:26:21 -0700

    Added AbstractClass Employee (and Touched This)

--- src/PaymentApplication/Commissioned.java
+++ src/PaymentApplication/Commissioned.java
@@ -1,30 +1,8 @@
 package PaymentApplication;
 
-public class Commissioned implements Payee {
+public class Commissioned extends Employee {
 
-	private String name;
-	private Double commAmount;
-	private Integer bankAccount; 
-	
-	public Commissioned (String name, Double commAmount, Integer bankAccount) {
-		this.name = name;
-		this.commAmount = commAmount;
-		this.bankAccount = bankAccount;
+	public Commissioned (String name, Double grossPay, Integer bankAccount) {
+		super(name, grossPay, bankAccount);
 	}
-	
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public Double getGrossPayment() {
-		return commAmount;
-	}
-
-	@Override
-	public Integer getBankAccount() {
-		return bankAccount;
-	}
-
 }
--- src/PaymentApplication/Salaried.java
+++ src/PaymentApplication/Salaried.java
@@ -1,33 +1,8 @@
 package PaymentApplication;
 
-public class Salaried implements Payee {
-	
-	private String name;
-	private Double grossPay;
-	private Integer bankAccount;
-	
+public class Salaried extends Employee {
+
 	public Salaried (String name, Double grossPay, Integer bankAccount) {
-		this.name = name;
-		this.grossPay = grossPay;
-		this.bankAccount = bankAccount;
+		super(name, grossPay, bankAccount);
 	}
-	
-	
-	@Override
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public Double getGrossPayment() {
-		return grossPay;
-	}
-
-	@Override
-	public Integer getBankAccount() {
-		return bankAccount;
-	}
-
-	
-
 }
